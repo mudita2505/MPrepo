7928beafcc175ba6cf177031093828fee8d7d97d
mudita2505 <88099249+mudita2505@users.noreply.github.com>
2021-08-20 11:02:39 +0530

    Update settings.py

--- minor/settings.py
+++ minor/settings.py
@@ -56,6 +56,22 @@ MIDDLEWARE = [
    
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "https://example.com",
+    "https://sub.example.com",
+    "http://localhost:8080",
+    "http://127.0.0.1:9000",
+]
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
 ROOT_URLCONF = 'minor.urls'
 
 TEMPLATES = [
